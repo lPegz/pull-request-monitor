6e08d5c7558e05b138c3081af54d9e4c7fd00b47
lPegz <pegorarohluiz@gmail.com>
2018-05-22 22:11:52 -0300

    Added more Logo animations in CSS, reformatted file indentation.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import pr_logo from './pr-logo.png';
+import rsx_logo from './logo.svg';
+import github_logo from './GitHub-Mark-Light-64px.png';
 import './scss/colors.css';
 import './App.css';
 
@@ -7,18 +9,20 @@ import OrganizationList from './components/Organizations';
 import RepositoriesSelected from './components/RepositoriesSelected';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <RepositoriesSelected />
-        <OrganizationList />
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={github_logo} className="GitHub-logo" alt="logo"/>
+                    <img src={pr_logo} className="PR-logo" alt="logo"/>
+                    <img src={rsx_logo} className="App-logo" alt="logo"/>
+                    <h1 className="App-title">Welcome to React</h1>
+                </header>
+                <RepositoriesSelected/>
+                <OrganizationList/>
+            </div>
+        );
+    }
 }
 
 export default App;
